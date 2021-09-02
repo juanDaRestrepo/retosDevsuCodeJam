654438e584ae3958b5fbf13fef69e448e9fd2187
DanielRestrepo <jdrestrepo@unitecnica.net>
2021-09-02 10:46:01 -0500

    retosPreparacionDevsuCodeJam

---
+++ retos/serie/serie.php
@@ -0,0 +1,27 @@
+<?php
+
+function hacerSumaSegunSerie($x,$y){
+
+$arraySerie=[];
+$primerN=7;
+$segundoN=6;
+
+for ($i=0;$i<=127;$i++){
+    
+    $arraySerie[]=$primerN;
+    $arraySerie[]=$segundoN;
+
+    $primerN=$primerN+1;
+    $segundoN=$segundoN-2;
+
+}
+if($x<1 || $x>255 || $y<0 || $y>255){
+    return -1;
+}else{
+    $suma=$arraySerie[$x-1]+$arraySerie[$y-1];
+    return $suma;
+}
+
+}
+echo hacerSumaSegunSerie(8,9);
+?>
